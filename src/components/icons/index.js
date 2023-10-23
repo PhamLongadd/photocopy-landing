86fc2e1e3af0977d40a225836b8971d3783e2e65
Pham Minh Tuan <phmtuan313@gmail.com>
2023-10-23 17:25:41 +0700

    update

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -41,7 +41,7 @@ export const WalletIcon = ({ className }) => (
     <path
       className={styles.textBrand9}
       fill="currentColor"
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M98 66v20a4 4 0 0 1-4 4H8.5A8.5 8.5 0 0 1 0 81.5v-63C0 13.8 3.8 10 8.5 10H82a4 4 0 0 1 4 4h4v3h4v4a4 4 0 0 1 4 4v21a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2zM2 18.5A6.5 6.5 0 0 0 8.5 25H94v1H8.5A7.5 7.5 0 0 1 2 22.24V81.5A6.5 6.5 0 0 0 8.5 88H94a2 2 0 0 0 2-2V66H80a10 10 0 1 1 0-20h16V25a2 2 0 0 0-2-2H8.5a4.5 4.5 0 1 1 0-9H84a2 2 0 0 0-2-2H8.5A6.5 6.5 0 0 0 2 18.5zM8.5 16a2.5 2.5 0 0 0 0 5H92v-2H8.5a.5.5 0 1 1 0-1H88v-2H8.5zM80 48a8 8 0 1 0 0 16h18V48H80zM8.5 29c-1.69 0-3.28-.4-4.7-1.1l.45-.9c1.28.64 2.72 1 4.25 1v1zm3.5-1h4v1h-4v-1zm12 0v1h-4v-1h4zm4 0h4v1h-4v-1zm12 0v1h-4v-1h4zm4 0h4v1h-4v-1zm12 0v1h-4v-1h4zm4 0h4v1h-4v-1zm12 0v1h-4v-1h4zm4 0h4v1h-4v-1zm8 0h4v1h-4v-1zm1 57h4v1h-4v-1zm-4 0v1h-4v-1h4zm-12 0h4v1h-4v-1zm-4 0v1h-4v-1h4zm-12 0h4v1h-4v-1zm-4 0v1h-4v-1h4zm-12 0h4v1h-4v-1zm-4 0v1h-4v-1h4zm-12 0h4v1h-4v-1zm-4 0v1h-4v-1h4zM8 38v24H7V38h1zm77 18a5 5 0 1 1-10 0 5 5 0 0 1 10 0zm-5 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"
     ></path>
   </svg>
@@ -69,13 +69,13 @@ export const ChipIcon = ({ className }) => (
     <path
       className={styles.textBrand3}
       fill="currentColor"
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M18 20c0-1.1.9-2 2-2h60a2 2 0 0 1 2 2v60a2 2 0 0 1-2 2H20a2 2 0 0 1-2-2V20z"
     ></path>
     <path
       className={styles.textBrand9}
       fill="currentColor"
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M6 0h88a6 6 0 0 1 6 6v88a6 6 0 0 1-6 6H6a6 6 0 0 1-6-6V6a6 6 0 0 1 6-6zm88 2H6a4 4 0 0 0-4 4v88a4 4 0 0 0 4 4h88a4 4 0 0 0 4-4V6a4 4 0 0 0-4-4zM6 4h88a2 2 0 0 1 2 2v88a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2zm88 1H6a1 1 0 0 0-1 1v88a1 1 0 0 0 1 1h88a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1zM71 82h-2v4h-1v3h-4v-3h-1v-4h-2v4h-1v3h-4v-3h-1v-4h-2v4h-1v3h-4v-3h-1v-4h-2v4h-1v3h-4v-3h-1v-4h-2v4h-1v3h-4v-3h-1v-4h-2v4h-1v3h-4v-3h-1v-4h-3a2 2 0 0 1-2-2v-3h-4v-1h-3v-4h3v-1h4v-2h-4v-1h-3v-4h3v-1h4v-2h-4v-1h-3v-4h3v-1h4v-2h-4v-1h-3v-4h3v-1h4v-2h-4v-1h-3v-4h3v-1h4v-2h-4v-1h-3v-4h3v-1h4v-2h-4v-1h-3v-4h3v-1h4v-3c0-1.1.9-2 2-2h3v-4h1v-3h4v3h1v4h2v-4h1v-3h4v3h1v4h2v-4h1v-3h4v3h1v4h2v-4h1v-3h4v3h1v4h2v-4h1v-3h4v3h1v4h2v-4h1v-3h4v3h1v4h2v-4h1v-3h4v3h1v4h3a2 2 0 0 1 2 2v3h4v1h3v4h-3v1h-4v2h4v1h3v4h-3v1h-4v2h4v1h3v4h-3v1h-4v2h4v1h3v4h-3v1h-4v2h4v1h3v4h-3v1h-4v2h4v1h3v4h-3v1h-4v2h4v1h3v4h-3v1h-4v3a2 2 0 0 1-2 2h-3v4h-1v3h-4v-3h-1v-4zM20 20v60h60V20H20zm9 8h43v44H28V28h1zm42 1H29v42h42V29zm-40 2h38v38H31V31zm1 1v36h36V32H32zm-8-6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm1-2a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm53 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm-2 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zM24 78a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm1-2a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm53 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm-2 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-1 9h-2v2h2v-2zm-10 2h2v-2h-2v2zm-6-2h-2v2h2v-2zm-10 2h2v-2h-2v2zm-6-2h-2v2h2v-2zm-10 2h2v-2h-2v2zm-8-2v2h2v-2h-2zM12 73v2h2v-2h-2zm2-6v-2h-2v2h2zm-2-10v2h2v-2h-2zm2-6v-2h-2v2h2zm-2-10v2h2v-2h-2zm2-6v-2h-2v2h2zm-2-10v2h2v-2h-2zm74 50h2v-2h-2v2zm2-8v-2h-2v2h2zm-2-10v2h2v-2h-2zm2-6v-2h-2v2h2zm-2-10v2h2v-2h-2zm2-6v-2h-2v2h2zm-2-8h2v-2h-2v2zM75 12h-2v2h2v-2zm-2 70v2h2v-2h-2zm-8 0v2h2v-2h-2zm-6 0h-2v2h2v-2zm-10 0v2h2v-2h-2zm-6 0h-2v2h2v-2zm-10 0v2h2v-2h-2zm-8 0v2h2v-2h-2zm-7-9h-2v2h2v-2zm0-6v-2h-2v2h2zm0-10h-2v2h2v-2zm0-6v-2h-2v2h2zm0-10h-2v2h2v-2zm0-6v-2h-2v2h2zm0-10h-2v2h2v-2zm64 50h2v-2h-2v2zm0-8h2v-2h-2v2zm0-10v2h2v-2h-2zm0-6h2v-2h-2v2zm0-10v2h2v-2h-2zm0-6h2v-2h-2v2zm0-8h2v-2h-2v2zm-7-9v-2h-2v2h2zm-10-4h2v-2h-2v2zm2 4v-2h-2v2h2zm-10-4h2v-2h-2v2zm2 4v-2h-2v2h2zm-10-4h2v-2h-2v2zm2 4v-2h-2v2h2zm-10-4h2v-2h-2v2zm2 4v-2h-2v2h2zm-10-4h2v-2h-2v2zm2 4v-2h-2v2h2zm-10-4h2v-2h-2v2zm2 4v-2h-2v2h2z"
     ></path>
   </svg>
@@ -114,7 +114,7 @@ export const SecurityIcon = ({ className }) => (
     <path
       className={styles.textBrand9}
       fill="currentColor"
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M94 60c0 11.04-8.15 23.7-18.21 28.28L50 100 24.21 88.28C14.15 83.7 6 71.04 6 59.99V11.77A87.6 87.6 0 0 1 50 0a87.6 87.6 0 0 1 44 11.77V60zM50 2A85.6 85.6 0 0 0 8 12.94v47.05c0 10.26 7.7 22.22 17.04 26.47L50 97.8l24.96-11.34C84.3 82.2 92 70.26 92 59.99V12.94A85.6 85.6 0 0 0 50 2zm0 4a81.62 81.62 0 0 1 38 9.32v44.67c0 8.7-6.77 19.23-14.7 22.83L50 93.4 26.7 82.8C18.78 79.22 12 68.7 12 60V15.32A81.62 81.62 0 0 1 50 6zM27.11 81.9L50 92.32l22.89-10.4C80.45 78.47 87 68.3 87 59.99V15.92A80.62 80.62 0 0 0 50 7a80.62 80.62 0 0 0-37 8.92V60c0 8.3 6.55 18.48 14.11 21.92zM15 60V17.14A78.64 78.64 0 0 1 50 9a78.64 78.64 0 0 1 35 8.15V60c0 7.53-6.09 16.98-12.94 20.1L50 90.1 27.94 80.1C21.09 76.98 15 67.5 15 59.99zm68 0V18.4A76.67 76.67 0 0 0 50 11a76.67 76.67 0 0 0-33 7.4V60c0 6.73 5.64 15.48 11.77 18.27L50 87.92l21.23-9.65C77.37 75.47 83 66.73 83 59.99zm-33 4a18 18 0 1 1 0-36 18 18 0 0 1 0 36zm16-18a16 16 0 1 0-32 0 16 16 0 0 0 32 0zm-29.04 5.11A14 14 0 0 1 50 32v1a13 13 0 0 0-12.13 17.69l-.9.42zM56 44h3v10H41V44h3v-2a6 6 0 0 1 12 0v2zm-14 9h16v-6H42v6zm16-8H42v1h16v-1zm-3-1v-2a5 5 0 1 0-10 0v2h2v-2a3 3 0 0 1 6 0v2h2zm-3 0v-2a2 2 0 0 0-4 0v2h4z"
     ></path>
   </svg>
@@ -142,7 +142,7 @@ export const SpeedIcon = ({ className }) => (
     <path
       className={styles.textBrand9}
       fill="currentColor"
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M42 60H18l.2-1 .2-1L29.8 1l.2-1h48l-.8 2-14.4 36-.8 2H82l-1.6 2-43.68 54.6L34 100l.85-4.27L41.6 62l.4-2zm17.45-19l.7-1.74L75.04 2H69.4L54.2 40l-.4 1h5.65zm12.27 1H52.32l.55-1.37L68.32 2H31.64l-11.2 56h24l-.48 2.4-4.35 21.73L71.71 42zM39.19 84.27l-1.62 8.06L77.84 42H73L39.19 84.27zM35 6h12l-.2 1H35.82l-2.8 14H32l3-15zm-4 20h1.02l-2 10H29l2-10z"
     ></path>
   </svg>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import DefaultLayout from "../components/layouts/defaultLayout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || DefaultLayout;
